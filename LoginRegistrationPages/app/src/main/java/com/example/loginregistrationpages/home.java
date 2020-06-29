e51fa8dc6dd2835efc781023c9ed1dc6cfc863fb
cuRIO_wiTs <venigallabhuvana@gmail.com>
2020-06-29 11:24:12 +0530

    android internship

---
+++ LoginRegistrationPages/app/src/main/java/com/example/loginregistrationpages/home.java
@@ -0,0 +1,15 @@
+package com.example.loginregistrationpages;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;import androidx.appcompat.app.AppCompatActivity;
+
+public class home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_main);
+    }
+
+}
---
+++ LoginRegistrationPages/app/src/main/java/com/example/loginregistrationpages/register.java
@@ -0,0 +1,17 @@
+package com.example.loginregistrationpages;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;import androidx.appcompat.app.AppCompatActivity;
+
+public class register extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_main);
+
+
+    }
+
+}
